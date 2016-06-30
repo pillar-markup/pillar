f45e00155074d7c2a71d834d1ca440c5fb706f06
ThibaultAr <thibault.arloing@hotmail.fr>
2016-06-30 13:23:06 +0200

    add creation of mustache executable

--- build-jenkins.sh
+++ build-jenkins.sh
@@ -66,7 +66,44 @@ set -f
 exec "\$PHARO_VM" "\$PILLAR_IMAGE" pillar "\$command" --baseDirectory="\$PWD" "\$@"
 EOF
 
+
+echo =====================================================
+echo = Create a mustache executable script
+echo =====================================================
+
+cat > mustache <<EOF
+#!/usr/bin/env bash
+
+# PHARO_VM=${PHARO_VM:-./pharo}
+# MUSTACHE_IMAGE=${MUSTACHE_IMAGE:-./Pharo.image}
+
+PHARO_VM=${PHARO_VM}
+# ./pharo has a higher priority than PHARO_VM env if it exists
+if [ -f pharo ]; then
+	PHARO_VM="./pharo"
+fi
+
+MUSTACHE_IMAGE=${MUSTACHE_IMAGE}
+# Pharo.image has a higher priority than MUSTACHE_IMAGE env if it exists
+if [ -f Pharo.image ]; then
+	MUSTACHE_IMAGE="./Pharo.image"
+fi
+
+option=$1
+shift
+
+# disable parameter expansion to forward all arguments unprocessed to the VM
+set -f
+
+# TODO: when Pharo knows how to return the correct "current working directory", we
+# can remove the --baseDirectory from here. The current solution is
+# problematic because it overrides any baseDirectory parameter of the
+# pillar.conf file.
+exec "$PHARO_VM" "$MUSTACHE_IMAGE" mustache "$option" --baseDirectory="$PWD" "$@"
+EOF
+
 chmod +x pillar
+chmod +x mustache
 
 zip -r $JOB_NAME.zip $JOB_NAME.image $JOB_NAME.changes pillar
 
