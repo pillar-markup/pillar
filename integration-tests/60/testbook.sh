268fb4c5e9035df50b4363df79c2bd391a0e241d
Guille Polito <guille@gotham.local>
2017-06-06 11:31:48 +0200

    fixed script path

--- integration-tests/60/testbook.sh
+++ integration-tests/60/testbook.sh
@@ -1,9 +1,15 @@
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+
+
 rm -rf build
 mkdir build
 cd build
 
 # Download Pillar
-sh downloadPillar.sh
+sh ${__dir}/downloadPillar.sh
 
 # Download Archetype
 
