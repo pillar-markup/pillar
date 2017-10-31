553b30ea42048e559d1846876c55041e02dd7cd1
Guille Polito <guillermopolito@gmail.com>
2017-10-31 17:49:56 +0100

    Make build script work

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,31 +8,23 @@ set -o xtrace
 
 # Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__builddir="${__dir}/build"
+__builddir="$(pwd)/build"
 
 PHARO_VERSION=61+vm
 
-rm -rf __builddir
-mkdir -p __builddir
-
-echo =====================================================
-echo = Downloading Pharo and loading Pillar
-echo =====================================================
-
+rm -rf ${__builddir} && mkdir -p ${__builddir} && cd ${__builddir}
 
 wget --quiet -O - get.pharo.org/${PHARO_VERSION} | bash
-$PHARO="./pharo Pharo.image"
+PHARO="./pharo Pharo.image"
 ${PHARO} eval --save "Metacello new baseline: 'Pillar'; repository: 'filetree://${__dir}/../src'; load"
 ${PHARO} config http://smalltalkhub.com/mc/NorbertHartl/Mustache/main ConfigurationOfMustache --install=bleedingEdge
-
-echo =====================================================
-echo = Create a pillar deployment archive
-echo =====================================================
 
 ${PHARO} eval --save "Deprecation raiseWarning: false."
 ${PHARO} eval --save "Deprecation showWarning: false."
 ${PHARO} eval --save "StartupPreferencesLoader allowStartupScript: false."
 
 # UserManager was remove on Pharo5. We need to check if there is an other way to do this.
-${PHARO} --save "PharoFilesOpener default unsetInformAboutReadOnlyChanges."
-${PHARO} --save "Smalltalk logFileName: '/tmp/pillar.log'"
+${PHARO} eval --save "PharoFilesOpener default unsetInformAboutReadOnlyChanges."
+${PHARO} eval --save "Smalltalk logFileName: '/tmp/pillar.log'"
+
+cp ${__dir}/wrappers/* ${__builddir}
