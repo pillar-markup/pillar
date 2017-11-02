bf851c86a80b7c99f2c37635640adddd987e1850
Guille Polito <guillermopolito@gmail.com>
2017-11-02 11:09:44 +0100

    adding integration tests

---
+++ scripts/integration-tests/testbook.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__builddir="${__dir}/build"
+
+${__dir}/build.sh
+
+
+# Download Archetype
+mkdir -p test && cd test
+${__builddir}/pillar archetype book
+
+# Build book
+make book
