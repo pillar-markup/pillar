fb41fbd1aeeebcd20de17aee57a62d8db478d70e
Guille Polito <guillermopolito@gmail.com>
2017-11-02 17:46:42 +0100

    Fix firamono download script: fixed paths

--- scripts/travis/ensure_fonts.sh
+++ scripts/travis/ensure_fonts.sh
@@ -1,3 +1,6 @@
+# Install Fira Mono as needed by pillar on travis
+# Follow instructions on the following gist: https://gist.github.com/muammar/a5ffb635eb7f532346a8e777b847f8a7
+
 if [[ ${TRAVIS_OS_NAME} == "osx"   ]]; then
   brew update;
   brew install basictex;
@@ -9,6 +12,7 @@ elif [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip";
   unzip FiraMono3206.zip;
   mkdir -p ~/.fonts
+  cp Fira*/WEB/TTF/*.ttf ~/.fonts;
   cp Fira*/OTF/Fira* ~/.fonts;
   fc-cache -fv;
   luaotfload-tool --update;
