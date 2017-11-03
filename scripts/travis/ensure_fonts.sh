c0ef8bb0668d2b2fd299a2c525bb59d9d4e9fff4
Guille Polito <guillermopolito@gmail.com>
2017-11-03 18:39:37 +0100

    Added gentium font

--- scripts/travis/ensure_fonts.sh
+++ scripts/travis/ensure_fonts.sh
@@ -1,18 +1,4 @@
 # Install Fira Mono as needed by pillar on travis
-# Follow instructions on the following gist: https://gist.github.com/muammar/a5ffb635eb7f532346a8e777b847f8a7
 
-if [[ ${TRAVIS_OS_NAME} == "osx"   ]]; then
-  wget https://github.com/mozilla/Fira/archive/4.202.zip;
-  unzip 4.202.zip;
-  font_dir="$HOME/Library/Fonts";
-  mkdir -p $font_dir;
-  cp Fira-4.202/otf/* $font_dir/;
-elif [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-  wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip";
-  unzip FiraMonoFonts3206.zip;
-  mkdir -p ~/.fonts
-  cp Fira*/WEB/TTF/*.ttf ~/.fonts;
-  cp Fira*/OTF/Fira* ~/.fonts;
-  fc-cache -fv;
-  # luaotfload-tool --update;
-fi
+tlmgr install fira
+tlmgr install gentium-tug
