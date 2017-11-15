9306ab6c5ea87dfe771579b5f3d6d0204675b2d6
Guillermo Polito <guillermopolito@gmail.com>
2017-11-15 10:13:19 -0300

    Making installation work for windows so far

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,13 +10,61 @@ set -o xtrace
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __builddir="$(pwd)/build"
 
-PHARO_VERSION=61+vm
+VM_BINARY_NAME="Pharo"
+VM_BINARY_NAME_LINUX="pharo"
+VM_BINARY_NAME_WINDOWS="PharoConsole"
+
+function get_platform_identifier() {
+  local TMP_OS=`uname | tr "[:upper:]" "[:lower:]"`
+  if [[ "{$TMP_OS}" = *darwin* ]]; then
+    echo "mac";
+  elif [[ "{$TMP_OS}" = *linux* ]]; then
+    echo "linux";
+  elif [[ "{$TMP_OS}" = *win* ]]; then
+    echo "win";
+  elif [[ "{$TMP_OS}" = *mingw* ]]; then
+    echo "win";
+  elif [[ "{$TMP_OS}" = *msys* ]]; then
+    echo "win";
+  else
+    echo "Unsupported OS";
+    exit 1;
+  fi
+}
+
+function get_vm_executable {
+  local OS=$(get_platform_identifier)
+  if [ "$OS" == "win" ]; then
+    echo `find . -name ${VM_BINARY_NAME_WINDOWS}.exe`
+  elif [ "$OS" == "mac" ]; then
+    echo `find . -name ${VM_BINARY_NAME}`
+  elif [ "$OS" == "linux" ]; then
+    echo `ls ${VM_BINARY_NAME_LINUX}`
+  fi
+}
 
 rm -rf "${__builddir}" && mkdir -p "${__builddir}" && cd "${__builddir}"
 
-wget --quiet -O - get.pharo.org/${PHARO_VERSION} | bash
-PHARO="./pharo Pharo.image"
-${PHARO} eval --save "Metacello new baseline: 'Pillar'; repository: 'filetree://${__dir}/../src'; load"
+PHARO_VERSION=61
+PLATFORM_IDENFIFIER=$(get_platform_identifier)
+
+wget --output-document=image.zip files.pharo.org/get-files/${PHARO_VERSION}/pharo.zip
+wget --output-document=sources.zip files.pharo.org/get-files/${PHARO_VERSION}/sources.zip
+wget --output-document=vm.zip files.pharo.org/get-files/${PHARO_VERSION}/pharo-${PLATFORM_IDENFIFIER}-stable.zip
+
+unzip "sources.zip" && rm "sources.zip"
+unzip "image.zip" && rm "image.zip"
+unzip "vm.zip" && rm "vm.zip"
+
+PHARO="$(get_vm_executable) Pharo.image"
+
+REPOSITORY_PATH=${__dir}/../src
+OS=$(get_platform_identifier)
+if [ "$OS" == "win" ]; then
+    REPOSITORY_PATH=$(cygpath $REPOSITORY_PATH --windows)
+fi
+
+${PHARO} eval --save "Metacello new baseline: 'Pillar'; repository: 'filetree://${REPOSITORY_PATH}'; load"
 
 ${PHARO} eval --save "Deprecation raiseWarning: false."
 ${PHARO} eval --save "Deprecation showWarning: false."
@@ -26,5 +74,5 @@ ${PHARO} eval --save "StartupPreferencesLoader allowStartupScript: false."
 ${PHARO} eval --save "PharoFilesOpener default unsetInformAboutReadOnlyChanges."
 ${PHARO} eval --save "Smalltalk logFileName: '/tmp/pillar.log'"
 
-cp "${__dir}/wrappers/*" "${__builddir}"
+cp "${__dir}/wrappers/"* "${__builddir}"
 cp -r "${__dir}/../archetypes" "${__builddir}/archetypes"
