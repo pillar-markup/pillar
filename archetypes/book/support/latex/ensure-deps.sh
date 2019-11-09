2c36c05c120259d338433568d4b2c18f5db2e28c
StéphaneDucasse <stephane.ducasse@inria.fr>
2019-11-09 10:29:19 +0100

    Create ensure-deps.sh

---
+++ archetypes/book/support/latex/ensure-deps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+xargs tlmgr install <<DEPS
+    ctablestack
+    environ
+    filehook
+    fira
+    fontspec
+    footmisc
+    gentium-tug
+    graphics
+    import
+    listings
+    luatexbase
+    memoir
+    multirow
+    opensans
+    pgf
+    polyglossia
+    ragged2e
+    tcolorbox
+    tools
+    trimspaces
+    ulem
+    varwidth
+    xcolor
+DEPS
