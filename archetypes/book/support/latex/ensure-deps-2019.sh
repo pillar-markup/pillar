85af55c9b98539792467650e0429cb8126b7fd67
StéphaneDucasse <stephane.ducasse@inria.fr>
2019-11-09 17:55:13 +0100

    adding support for 2019/2018 and specific travis files.

--- archetypes/book/support/latex/ensure-deps-2019.sh
+++ archetypes/book/support/latex/ensure-deps-2019.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# this dependency list is for LaTex 2018
-
+# this dependency list is for LaTeX 2019
 xargs tlmgr install <<DEPS
     ctablestack
     environ
@@ -10,17 +9,15 @@ xargs tlmgr install <<DEPS
     footmisc
     gentium-tug
     graphics
-    ifetex
     import
     listings
-    luaotfload
     luatexbase
     memoir
-    ms
     multirow
     opensans
     pgf
     polyglossia
+    ragged2e
     tcolorbox
     tools
     trimspaces
