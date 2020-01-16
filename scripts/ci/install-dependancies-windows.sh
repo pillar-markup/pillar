b61fb0dfbf08f62a7935985b7f252182ea63b2df
Santiago Bragagnolo <santiagobragagnolo@gmail.com>
2020-01-16 15:39:19 +0000

    Adds a function to decide which folder of wrappers to copy (windows need some different details). Adds a building script for installing packages and fonts in windows.  Modifies build script to use 32 bits in the case of windows. It defaults to Pharo 8.

---
+++ scripts/ci/install-dependancies-windows.sh
@@ -0,0 +1,14 @@
+
+mpm --install fira
+mpm --install gentium-tug
+mpm --install opensans
+
+mpm --install varwidth
+mpm --install footmisc
+mpm --install tcolorbox
+mpm --install environ
+mpm --install trimspaces
+mpm --install ctablestack
+mpm --install import
+mpm --install multirow
+mpm --install ifetex
